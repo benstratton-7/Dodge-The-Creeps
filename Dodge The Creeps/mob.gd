2de18fb22f7d35f817566a71a239fb614bff1cbe
benstratton-7 <109835020+benstratton-7@users.noreply.github.com>
2023-10-19 10:52:10 -0400

    <--Here

--- Dodge The Creeps/mob.gd
+++ Dodge The Creeps/mob.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time
 func _ready():
 	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
